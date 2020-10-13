be4cd92bc4b6871846c35d91eac5d3c746191ffb
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-13 11:46:04 -0700

    adds integration test for posting and checking correct GET value adds mutex to InMemoryStore

--- server/server.go
+++ server/server.go
@@ -4,7 +4,26 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"sync"
 )
+
+type InMemoryPlayerStore struct {
+	store map[string]int
+	mu    sync.Mutex
+}
+
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{store: map[string]int{}}
+}
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mu.Lock()
+	i.store[name]++
+	i.mu.Unlock()
+}
 
 type PlayerStore interface {
 	GetPlayerScore(name string) int
@@ -20,19 +39,18 @@ func (p *PlayerServer) SetStore(pl PlayerStore) {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
 	switch r.Method {
 	case http.MethodPost:
-		p.processWin(w, r)
+		p.processWin(w, player)
 	case http.MethodGet:
-		p.showScore(w, r)
+		p.showScore(w, player)
 	}
 
 }
 
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.store.GetPlayerScore(player)
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
@@ -40,8 +58,7 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
 	p.store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
