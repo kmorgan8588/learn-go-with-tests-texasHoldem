56259a3ab02dd4641acb09c113ce6b30d0319c17
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-15 14:34:06 -0700

    introduces tape struct to remove Seek on writes and reads

--- server/server_filesystem.go
+++ server/server_filesystem.go
@@ -6,14 +6,14 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.ReadWriteSeeker
+	database io.Writer
 	league   League
 }
 
 func NewFileSystemPlayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStore {
 	database.Seek(0, 0)
 	league, _ := NewLeague(database)
-	return &FileSystemPlayerStore{database, league}
+	return &FileSystemPlayerStore{&tape{database}, league}
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
@@ -38,7 +38,6 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 		f.league = append(f.league, Player{name, 1})
 	}
 
-	f.database.Seek(0, 0)
 	json.NewEncoder(f.database).Encode(f.league)
 }
 
