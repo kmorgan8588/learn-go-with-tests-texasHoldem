daa2bd62a14b79999d9004c40c0502774202e309
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-19 12:07:09 -0700

    adding new route /game and supporting tests

--- server/server_test.go
+++ server/server_test.go
@@ -89,6 +89,19 @@ func TestStoreWins(t *testing.T) {
 	})
 }
 
+func TestGame(t *testing.T) {
+	t.Run("GET /game returns 200", func(t *testing.T) {
+		server := NewPlayerServer(&StubPlayerStore{})
+
+		request, _ := http.NewRequest(http.MethodGet, "/game", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
+
 func newPostWinRequest(name string) *http.Request {
 	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
 	return req
